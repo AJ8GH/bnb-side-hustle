a73cb48a48eaef7af3146f1562920362c8c4ad8a
Adam Jonas <adamjonas25@gmail.com>
2021-03-02 21:56:17 +0000

    adds user authentication

--- lib/user.rb
+++ lib/user.rb
@@ -1,16 +1,26 @@
 class User
   attr_reader :id, :name, :email, :password
 
-  def self.create(name:, email:, password:)
-    user = DatabaseConnection.query("INSERT INTO users (name, email, password)
-    VALUES ('#{name}', '#{email}', '#{password}') RETURNING *;")
+  class << self
+    def create(name:, email:, password:)
+      user = DatabaseConnection.query("INSERT INTO users (name, email, password)
+      VALUES ('#{name}', '#{email}', '#{password}') RETURNING *;")
 
-    new(user[0]['id'], user[0]['name'], user[0]['email'], user[0]['password'])
-  end
+      new(user[0]['id'], user[0]['name'], user[0]['email'], user[0]['password'])
+    end
 
-  def self.find(id:)
-    user = DatabaseConnection.query("SELECT * FROM users WHERE id = #{id};")
-    new(user[0]['id'], user[0]['name'], user[0]['email'], user[0]['password'])
+    def find(id:)
+      user = DatabaseConnection.query("SELECT * FROM users WHERE id = #{id};")
+      new(user[0]['id'], user[0]['name'], user[0]['email'], user[0]['password'])
+    end
+
+    def authenticate(email:, password:)
+      result = DatabaseConnection.query("SELECT * FROM users
+        WHERE email = '#{email}';").first
+      return unless result
+      return unless result['password'] == password
+      new(result['id'], result['name'], result['email'], result['password'])
+    end
   end
 
   def initialize(id, name, email, password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 describe User do
   describe '.create' do
     it 'creates a new user' do
-      user = User.create(name: 'name', email: 'email', password: 'password')
+      user = described_class.create(name: 'name', email: 'email', password: 'password')
+
       expect(user.name).to eq('name')
       expect(user.email).to eq('email')
       expect(user.password).to eq('password')
@@ -10,11 +11,37 @@ describe User do
 
   describe '.find' do
     it 'finds a user' do
-      user = User.create(name: 'name', email: 'email', password: 'password')
-      user2 = User.find(id: user.id)
+      user = described_class.create(name: 'name', email: 'email', password: 'password')
+      user2 = described_class.find(id: user.id)
+
       expect(user2.name).to eq(user.name)
       expect(user2.id).to eq(user.id)
       expect(user2.email).to eq(user.email)
+    end
+  end
+
+  describe '.authenticate' do
+    it 'returns the user on successful sign in' do
+      user = described_class.create(name: 'name', email: 'email', password: 'password')
+      authenticated = described_class.authenticate(email: 'email', password: 'password')
+
+      expect(authenticated.id).to eq user.id
+      expect(authenticated.name).to eq user.name
+      expect(authenticated.email).to eq user.email
+    end
+
+    it 'returns nil for invalid email' do
+      user = described_class.create(name: 'name', email: 'wrong_email', password: 'password')
+      authenticated = described_class.authenticate(email: 'email', password: 'password')
+
+      expect(authenticated).to be_nil
+    end
+
+    it 'returns nil for wrong password' do
+      user = described_class.create(name: 'name', email: 'email', password: 'wrong_password')
+      authenticated = described_class.authenticate(email: 'email', password: 'password')
+
+      expect(authenticated).to be_nil
     end
   end
 end
